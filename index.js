9591b357e56954f31596adf6b1604ba9542f409f
maaz7213 <maaz0301301@gmail.com>
2024-10-07 14:27:24 +0500

    first commit

--- index.js
+++ index.js
@@ -456,7 +456,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Start the server on the specified port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
