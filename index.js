a02bad6c4e51688d8305b5bbb7f4d41d38755417
maaz7213 <maaz0301301@gmail.com>
2024-10-07 14:55:26 +0500

    first commit

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const app = express();
 // Use CORS middleware
 app.use(cors());
 app.use(express.json());  // Middleware to parse JSON request bodies
+app.use(cors({
+  origin: ['https://dashboard-fiverr-nodejs.vercel.app'], // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 const mongoURI = 'mongodb+srv://shiwanshaggarwal2004:YPvS4SDJwKc59iUv@cluster0.ueomq.mongodb.net/';
 const dbName = 'test';
